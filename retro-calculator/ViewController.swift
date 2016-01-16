18fdf76be3edb7cca9d01f21668abeff1611f8bd
Ahmed Zouari <asz130230@utdallas.edu>
2016-01-16 11:30:28 -0600

    Retro Calculator implementation

--- retro-calculator/ViewController.swift
+++ retro-calculator/ViewController.swift
@@ -7,12 +7,115 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Substract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    
+    var currentOperation: Operation = Operation.Empty
+    var result = ""
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+            
+        } catch let err as NSError {
+            print ( err.debugDescription )
+        }
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        playSound()
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+        
+        
+    }
+    
+    
+    func processOperation(op: Operation) {
+        playSound()
+        
+        if currentOperation != Operation.Empty {
+            //Run some math
+            
+            if runningNumber != "" {
+            rightValStr = runningNumber
+            runningNumber = ""
+            
+            if currentOperation == Operation.Multiply {
+                result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                
+            } else if currentOperation == Operation.Divide {
+                result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                
+            } else if currentOperation == Operation.Add {
+                result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                
+            } else if currentOperation == Operation.Substract {
+                result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                
+            }
+            
+            leftValStr = result
+            outputLbl.text = result
+                
+            }
+            
+            currentOperation = op
+            
+        } else {
+            //This is the first time an operator has been pressed
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+        }
+        
+    }
+    
+    func playSound() {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +124,54 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+        
+    }
+    
+    
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+        
+    }
+    
+    
+    
+    @IBAction func onSubstractPressed(sender: AnyObject) {
+        processOperation(Operation.Substract)
+        
+    }
+    
+    
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+        
+        
+    }
+    
+    
+    
+    @IBAction func onEqualAction(sender: AnyObject) {
+        processOperation(currentOperation)
+        
+    }
+    
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        btnSound.play()
+        currentOperation = Operation.Empty
+        outputLbl.text = "0"
+        
+        
+    }
+        
+    
+    
+    
+    
+    
+    
 }
 
